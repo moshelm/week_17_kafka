3adf85c15850c08c2648d7b2be32e13465787ad5
Moshe <moshe@example.com>
2026-02-12 11:48:03 +0200

    add: basic

---
+++ consumer/app/kafka_consumer.py
@@ -0,0 +1,38 @@
+import json
+import os 
+from confluent_kafka import Consumer
+from mysql_connection import Mysql_Manger
+
+TOPIC_KAFKA = os.getenv("TOPIC_KAFKA","init_mongo")
+PRODUCER_CONFIG = os.getenv('PRODUCER_CONFIG',"localhost:9092")
+    
+CONSUMER_CONFIG = {
+    "bootstrap.servers": PRODUCER_CONFIG,
+    "group.id": "order-tracker",
+    "auto.offset.reset": "earliest"
+}
+mysql = Mysql_Manger()
+
+consumer = Consumer(CONSUMER_CONFIG)
+
+consumer.subscribe([TOPIC_KAFKA])
+
+print("🟢 Consumer is running and subscribed to orders topic")
+
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("❌ Error:", msg.error())
+            continue
+        
+        value = msg.value().decode("utf-8")
+        order = json.loads(value)
+        print(f"📦 Received order: {order['quantity']} x {order['item']} from {order['user']}")
+except KeyboardInterrupt:
+    print("\n🔴 Stopping consumer")
+
+finally:
+    consumer.close()
