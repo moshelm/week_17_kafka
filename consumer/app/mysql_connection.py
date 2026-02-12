0ca5d53b2701fe289d1dc32f711c0a505a399dee
Moshe <moshe@example.com>
2026-02-12 11:48:16 +0200

    fix: class oop

--- consumer/app/mysql_connection.py
+++ consumer/app/mysql_connection.py
@@ -15,56 +15,57 @@ CONFIG_DB = {
     'password':MYSQL_PASSWORD,
     # 'database': MYSQL_DATABASE
 }
-conn = connection.MySQLConnection(**CONFIG_DB)
-cursor = conn.cursor()
+class Mysql_Manger():
+    def __init__(self):
+        self.conn = connection.MySQLConnection(**CONFIG_DB)
 
-def create_database():
-    with conn.cursor() as cursor:
-        cursor.execute(f'CREATE DATABASE IF NOT EXISTS {MYSQL_DATABASE};')
-        conn.commit()
-        cursor.close()
-        
+    def create_database(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f'CREATE DATABASE IF NOT EXISTS {MYSQL_DATABASE};')
+            self.conn.commit()
+            cursor.close()
+            
 
-def create_table_costumers():
-    with conn.cursor() as cursor:
-        query = f"""USE {MYSQL_DATABASE};
-        CREATE TABLE IF NOT EXISTS costumers (
-        customerNumber INT AUTO_INCREMENT PRIMARY KEY,
-customerName VARCHAR(255),
-    contactLastName VARCHAR(255),
-    contactFirstName VARCHAR(255),
-    phone INT,
-    addressLine1 VARCHAR(255),
-    addressLine2 VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    postalCode INT,
-    country VARCHAR(255),
-    salesRepEmployeeNumber INT,
-    creditLimit FLOAT ) 
-        """
-        cursor.execute(query)
-        conn.commit()
-        cursor.close()
-        
+    def create_table_costumers(self):
+        with self.conn.cursor() as cursor:
+            query = f"""USE {MYSQL_DATABASE};
+            CREATE TABLE IF NOT EXISTS costumers (
+            customerNumber INT AUTO_INCREMENT PRIMARY KEY,
+    customerName VARCHAR(255),
+        contactLastName VARCHAR(255),
+        contactFirstName VARCHAR(255),
+        phone INT,
+        addressLine1 VARCHAR(255),
+        addressLine2 VARCHAR(255),
+        city VARCHAR(255),
+        state VARCHAR(255),
+        postalCode INT,
+        country VARCHAR(255),
+        salesRepEmployeeNumber INT,
+        creditLimit FLOAT ) 
+            """
+            cursor.execute(query)
+            self.conn.commit()
+            cursor.close()
+            
 
-def create_table_orders():
-    with conn.cursor() as cursor:
-        query = f"""USE {MYSQL_DATABASE};
-        CREATE TABLE IF NOT EXISTS orders
-        (id INT AUTO_INCREMENT PRIMARY KEY, 
-    orderNumber INT,
-    orderDate DATE,
-    requiredDate DATE,
-    shippedDate DATE,
-    status VARCHAR(255),
-    comments VARCHAR(255),
-    customerNumber INT,
-    FOREIGN KEY (customerNumber) REFERENCES costumers(customerNumber) )
-        """
-        cursor.execute(query)
-        conn.commit()
-        cursor.close()
+    def create_table_orders(self):
+        with self.conn.cursor() as cursor:
+            query = f"""USE {MYSQL_DATABASE};
+            CREATE TABLE IF NOT EXISTS orders
+            (id INT AUTO_INCREMENT PRIMARY KEY, 
+        orderNumber INT,
+        orderDate DATE,
+        requiredDate DATE,
+        shippedDate DATE,
+        status VARCHAR(255),
+        comments VARCHAR(255),
+        customerNumber INT,
+        FOREIGN KEY (customerNumber) REFERENCES costumers(customerNumber) )
+            """
+            cursor.execute(query)
+            self.conn.commit()
+            cursor.close()
         
 
 # # create_database()
